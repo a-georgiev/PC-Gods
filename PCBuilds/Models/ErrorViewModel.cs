0bbc410896e806fe6351e9d0c358d9d533d7ac72
Aleksandar Georgiev <iam.a.georgiev@gmail.com>
2022-03-20 23:54:18 +0200

    asd

--- PCBuilds/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PCBuilds.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
