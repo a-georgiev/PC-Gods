1f27225f6a03fcce57eab8cce7d5b14b084f20fc
a-georgiev <iam.a.georgiev@gmail.com>
2023-05-08 15:54:45 +0300

    infrastructure initial

---
+++ PCBuilds.Infrastructure/Data/Components/Cooler.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static PCBuilds.Infrastructure.Data.DataConstants;
+
+namespace PCBuilds.Infrastructure.Data.Components
+{
+    public class Cooler: Component
+    {
+        [Required]
+        [MaxLength(ModelMaxLen)]
+        public string Model { get; set; }
+
+        [Required]
+        public int MinRpm{ get; set; }
+
+        [Required]
+        public int MaxRpm { get; set; }
+
+        [Required]
+        public int Noise { get; set; }
+
+        [Required]
+        [MaxLength(SocketMaxLen)]
+        public string Socket { get; set; }
+
+    }
+}
---
+++ PCBuilds.Infrastructure/Data/PCBuildDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PCBuilds.Infrastructure.Data;
+using PCBuilds.Infrastructure.Data.Components;
+
+namespace PCBuilds.Data
+{
+    public class PCBuildDbContext : IdentityDbContext
+    {
+        public PCBuildDbContext()
+        {
+
+        }
+        public DbSet<Build> Builds{ get; set; }
+        public DbSet<Cpu> Cpus{ get; set; }
+        public DbSet<Cooler> Coolers{ get; set; }
+        public DbSet<Memmory> Memmories{ get; set; }
+        public DbSet<Motherboard> Motherboards{ get; set; }
+        public DbSet<PowerSupply> PowerSupplies{ get; set; }
+        public DbSet<Storage> Storages { get; set; }
+        public DbSet<VideoCard> VideoCards { get; set; }
+
+        public PCBuildDbContext(DbContextOptions<PCBuildDbContext> options)
+            : base(options)
+        {
+
+        }
+
+    }
+}
